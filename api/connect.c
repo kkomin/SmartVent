3b2e2070e38435f58e391f1de0b5efd3bc511af9
kkomin <min_0625@kakao.com>
2025-07-28 09:30:21 +0900

    [⚙️ FEATURE] : 메모리, libcurl 초기화

--- api/connect.c
+++ api/connect.c
@@ -10,12 +10,19 @@ struct Memory {
 };
 
 int main() {
+    CURL *curl;             // CURL 핸들
+    CURLcode res;           // CURL 요청 결과
+    struct Memory chunk;    // 응답 저장 구조체
+
     // 메모리 초기화
+    chunk.response = malloc(1);
+    chunk.size = 0;
 
     // libcurl 초기화
+    curl_global_init(CURL_GLOBAL_DEFAULT);      // curl 라이브러리 전체 초기화
+    curl = curl_easy_init();                    // 요청 객체 생성
 
     // api url 설정
-    
 
     // 요청 옵션 설정
 
