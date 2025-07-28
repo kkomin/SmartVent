16fe933aae8ca52a1fbd5e0158d7d8e308af0a56
kkomin <min_0625@kakao.com>
2025-07-28 14:49:39 +0900

    [🆕 NEW] : 미세먼지 API 파싱

---
+++ api/air_parse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void parse_air_json(const char *json_str) {
+    // json 파싱
+
+    // list 배열에서 첫 번째 요소 가져오기
+
+    // 해당 요소의 components 객체 추출
+
+    // components 내 pm2_5, pm10 값 읽기
+    
+    // 읽은 값 변수에 저장하거나 출력
+}
