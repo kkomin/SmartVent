a4332b445681b72420ee5c37f58d80eb74743bd0
kkomin <min_0625@kakao.com>
2025-07-29 22:04:52 +0900

    [📝 DOCUMENT] : 시리얼 데이터 수신 및 파싱 과정 작성

---
+++ util/serial_read.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "weather_data.h"
+
+// 시리얼 데이터 받아오기 (시리얼 포트 열기)
+
+// 시리얼 통신 속성 설정
+
+// 데이터 수신
+
+// 문자열 파싱
+
+// weatherdata.h의 구조체에 tmp_in, hum_in 저장
+
+// 시리얼 포트 닫기
+
+// ++ 실패 처리 ++
+
+// 이후 이 데이터 기반으로 auto_vent()함수에 인자로 넘기기
