d21eca0cb884e84967db1611081cdab155dafb35
kkomin <min_0625@kakao.com>
2025-07-29 14:14:44 +0900

    [⚙️ FEATURE] : DB에 현재 상태 저장 로직 호출

--- src/main.c
+++ src/main.c
@@ -22,7 +22,9 @@ void main() {
 
     // DB 저장 -> 파싱한 데이터, 타임스탬프 -> environment_data 테이블에 저장
     // MYSQL *conn, const char *timestamp, float tmp_in, float hum_in, float tmp_out, float hum_out, float pm25, float pm10, const char *weather_desc, int vent_status
-    save_environment_data();
+    
+    vent_status = 1; // 예시: 환기 상태 1 (환기 중) -> 추후 조건에 따른 환기 정보로 변수 변경될 예정 (여기서 삭제 예정)
+    save_environment_data(conn, timestamp, weather_data.tmp_in, weather_data.hum_in, weather_data.tmp_out, weather_data.hum_out, air_data.pm25, air_data.pm10, weather_data.weather_desc, vent_status);
 
     // 10. 시스템 로그 저장 -> 저장 성공 여부 및 동작 로그 -> system_logs 테이블에 기록
 
