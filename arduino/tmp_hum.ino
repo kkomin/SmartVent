6db8003fb5c9661b8167dc0d5a02d56f0c8f0caa
kkomin <min_0625@kakao.com>
2025-07-29 21:30:15 +0900

    [⚙️ FEATURE] : 센서로부터 온습도 읽고 수신상태 처리

--- arduino/tmp_hum.ino
+++ arduino/tmp_hum.ino
@@ -1,7 +1,7 @@
 #include <DHT.h>
 
 // DHT 센서 핀 번호 및 종류 정의
-#define DHTTPYE DHT11
+#define DHTTYPE DHT11
 #define DHTPIN 2
 #define LEDPIN 13
 
@@ -12,7 +12,7 @@ void setup() {
     // Serial 통신 시작
     Serial.begin(9600);
     // DHT 센서 시작
-    dnt.begin();
+    dht.begin();
     // LED 핀을 출력으로 설정
     pinMode(LEDPIN, OUTPUT);
     digitalWrite(LEDPIN, LOW);  // 초기 LED OFF
@@ -20,15 +20,30 @@ void setup() {
 
 void loop() {
     // DHT 센서로부터 온도와 습도 읽기
-    // 온도와 습도를 Serial로 전송
-    // 일정 시간 대기
+    float tmp = dht.readTemperature();
+    float hum = dht.readHumidity();
+
+    if(isnan(tmp) || isnan(hum)) {
+        Serial.println("DHT11 센서 오류 발생");
+    } else {
+        // 온도와 습도를 Serial로 전송
+        Serial.print(tmp, 2);
+        Serial.print(", ");
+        Serial.print(hum, 2);
+    }
     
     // 라즈베리파이에서 auto_vent()함수로 판단된 결과 전송
     // 1이면 환기 상태 ON, 0이면 OFF
-    // Serial로 수신 처리
+    // Serial로 수신 처리 -> 환기 상태 수신 시
+    if(Serial.available() > 0) {
+        char state = Serial.read();
+        if(state == '1') {
+            digitalWrite(LEDPIN, HIGH);     //  LED ON
+        } else if (state == '0') {
+            digitalWrite(LEDPIN, LOW);      // LED OFF
+        }
+    }
     
-    // 환기 상태 수신 시
-    // 1 -> LED ON
-    // 0 -> LED OFF
-    
+    // 일정 시간 (5분마다 측정 및 상태 확인)
+    delay(300000);
 }
