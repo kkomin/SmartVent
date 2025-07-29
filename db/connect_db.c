80cf7e26780e2eef3111388b0848db2be0231577
kkomin <min_0625@kakao.com>
2025-07-29 09:47:01 +0900

    [🔐 SECURITY] : DB 연결 로직 보안 처리

--- db/connect_db.c
+++ db/connect_db.c
@@ -5,16 +5,24 @@
 MYSQL *conn;
 
 void connect_db() {
+    char host[100], user[100], pass[100], db[100];
+    unsigned int port = 0;
+
+    // db 정보 가져오기
+    get_db_config(host, user, pass, db, port);
+
     conn = mysql_init(NULL);
     if(conn == NULL) {
         fprintf(stderr, "mysql init() 실패\n");
         exit(1);
     }
 
-    if(mysql_real_connect(conn, "localhost", "root", "비밀번호", "SmartVent", 3306, NULL, 0) == NULL) {
+    if(mysql_real_connect(conn, host, user, pass, db, port, NULL, 0) == NULL) {
         fprintf(stderr, "mysql_real_connect() 실패 : %s\n", mysql_error(conn));
         mysql_close(conn);
         exit(1);
     }
-    printf("연결 성공!\n");
+
+    printf("db 연결 성공!\n");
+    return conn;
 }
